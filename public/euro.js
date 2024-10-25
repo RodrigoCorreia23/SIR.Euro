86503d81c474517a5eddff4ea9cc9fbee3ea3c47
RodrigoCorreia23 <rodrigo2003@live.com.pt>
2024-10-25 11:16:39 +0100

    alteracao6

--- public/euro.js
+++ public/euro.js
@@ -36,7 +36,7 @@ function consomeChaveJSON(chavejson) {
 
 function gerachave(e) {
 
-    fetch('https://sir-euro-7.onrender.com//euro')
+    fetch('https://sir-euro-7.onrender.com/euro')
         .then((res => res.json()))
         .then((data) => {
             updatechave(data);
